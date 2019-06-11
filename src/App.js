634c56f7d254657842743c39ce083de5c44f39a4
smrbts <samueljustinroberts@gmail.com>
2019-06-10 20:11:43 -0500

    SetUp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
+import VideoContainer from './VideoContainer'
 
-class App extends Component {
-  render() {
-    return <div>App component</div>;
+class App extends Component 
+{
+  render() 
+  {
+    return <div> <VideoContainer/> </div>;
   }
 }
 
---
+++ src/VideoContainer.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import keys from './keys';
+import SearchBar from './SearchBar';
+import VideoCard from './VideoCard';
+import VideoList from './VideoList';
+import _ from "lodash";
+
+export default class VideoContainer extends Component
+{
+    render()
+    {
+        return(
+            <div>
+
+            </div>
+            )
+    }
+}
